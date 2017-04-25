3d8a390dac0a5eed8cdef143923107dceaaa1c08
yuuyakasturai <ash_to_sakura@yahoo.co.jp>
2017-04-25 05:18:46 +0000

    for heroku

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,56 @@
+ass MessagesController < ApplicationController
+  before_action :set_message, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @messages = Message.all
+  end
+
+  def show
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+
+    if @message.save
+      flash[:success] = 'Message が正常に投稿されました'
+      redirect_to @message
+    else
+      flash.now[:danger] = 'Message が投稿されませんでした'
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @message.update(message_params)
+      flash[:success] = 'Message は正常に更新されました'
+      redirect_to @message
+    else
+      flash.now[:danger] = 'Message は更新されませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+    @message.destroy
+
+    flash[:success] = 'Message は正常に削除されました'
+    redirect_to messages_url
+  end
+
+  private
+
+  def set_message
+    @message = Message.find(params[:id])
+  end
+
+  def message_params
+    params.require(:message).permit(:content, :title)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
+  get 'messages/:id', to: 'messages#show'
+  post 'messages', to: 'messages#create'
+  put 'messages/:id', to: 'messages#update'
+  delete 'messages/:id', to: 'messages#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # index: show の補助ページ
+  get 'messages', to: 'messages#index'
+
+  # new: 新規作成用のフォームページ
+  get 'messages/new', to: 'messages#new'
+
+  # edit: 更新用のフォームページ
+  get 'messages/:id/edit', to: 'messages#edit'
 end
+
+#上記ソースコードの省略版
+
+#Rails.application.routes.draw do
+#  root to: 'messages#index'
+#  resources :messages
+#end
